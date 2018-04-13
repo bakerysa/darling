b9156c7b133103a0da9e7215695ed8729134d232
b_b0t <byronpolley@me.com>
2018-04-13 09:37:09 +0200

    Added custom post types

--- wp-content/themes/darling/functions.php
+++ wp-content/themes/darling/functions.php
@@ -176,28 +176,63 @@ function create_post_type() {
 	/**
 	 * Directors Post Type
 	 */
-  register_post_type( 'Directors',
+  register_post_type( 'Videos',
     array(
       'labels' => array(
-        'name' => __( 'Directors' ),
-        'singular_name' => __( 'Director' )
+        'name' => __( 'Videos' ),
+        'singular_name' => __( 'Video' )
       ),
       'public' => true,
 			'menu_icon'   => 'dashicons-video-alt2',
 			'supports' => array( 'title', 'editor', 'thumbnail', 'cat' ),
-			'taxonomies' => array( 'category' ),
+			'taxonomies' => array( 'director' ),
     )
   );
 
 	register_taxonomy(
-    		'Directors',
+    		'directors',
     		array(
     			'hierarchical'  => false,
-    			'label'         => __( 'Taste', CURRENT_THEME ),
-    			'singular_name' => __( 'Taste', CURRENT_THEME ),
+    			'label'         => __( 'Directors', CURRENT_THEME ),
+    			'singular_name' => __( 'Director', CURRENT_THEME ),
     			'rewrite'       => true,
     			'query_var'     => true
     		)
     	);
 }
 add_action( 'init', 'create_post_type' );
+
+/**
+ * Add custom taxonomies
+ *
+ * Additional custom taxonomies can be defined here
+ * http://codex.wordpress.org/Function_Reference/register_taxonomy
+ */
+function add_custom_taxonomies() {
+  // Add new "Locations" taxonomy to Posts
+  register_taxonomy('location', 'post', array(
+    // Hierarchical taxonomy (like categories)
+    'hierarchical' => true,
+    // This array of options controls the labels displayed in the WordPress Admin UI
+    'labels' => array(
+      'name' => _x( 'Locations', 'taxonomy general name' ),
+      'singular_name' => _x( 'Location', 'taxonomy singular name' ),
+      'search_items' =>  __( 'Search Locations' ),
+      'all_items' => __( 'All Locations' ),
+      'parent_item' => __( 'Parent Location' ),
+      'parent_item_colon' => __( 'Parent Location:' ),
+      'edit_item' => __( 'Edit Location' ),
+      'update_item' => __( 'Update Location' ),
+      'add_new_item' => __( 'Add New Location' ),
+      'new_item_name' => __( 'New Location Name' ),
+      'menu_name' => __( 'Locations' ),
+    ),
+    // Control the slugs used for this taxonomy
+    'rewrite' => array(
+      'slug' => 'locations', // This controls the base slug that will display before each term
+      'with_front' => false, // Don't display the category base before "/locations/"
+      'hierarchical' => true // This will allow URL's like "/locations/boston/cambridge/"
+    ),
+  ));
+}
+add_action( 'init', 'add_custom_taxonomies', 0 );
